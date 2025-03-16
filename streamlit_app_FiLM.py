84ae01033c5c4e9529db1e323ed56e1e578ba9fb
Tristan Quétin <tristanq@hotmail.fr>
2025-03-16 04:12:55 +0100

    corps d'appli streamlit

---
+++ streamlit_app_FiLM.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+def FiLM_visualization(query):
+    ### To be implemented ###
+    
+    return()
+
+
+st.title("Feature-wise Linear Modulations")
+
+tab1, tab2 = st.tabs(["Training", "Visualizing"])
+
+# Training tab
+with tab1:
+    st.header("Training")
+    epoch = st.slider("Epoch", 1, 20, 1)
+    model_choice = st.selectbox("Model", ["resnet", "raw"])
+    if st.button("Train"):
+        st.write(f"Training started with {model_choice} for {epoch} epochs")
+
+# Visualizing tab
+with tab2:
+    st.header("Visualizing")
+    query = st.text_input("Query")
+    if query:
+        image = FiLM_visualization(query)
+        st.image(image, caption=f"FiLMed image for query : {query}")
