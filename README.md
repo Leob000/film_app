a4297b34eb9663eddacb353c0b0e37eca6efab7f
Tristan Quétin <tristanq@hotmail.fr>
2025-03-22 08:05:52 +0100

    Add dataset creation scripts for small model in Streamlit and Readme instructions

--- README.md
+++ README.md
@@ -54,3 +54,9 @@ To run the model (on `CLEVR_val_000017.png` by default):
 ```bash
 sh scripts/run_model.sh
 ```
+
+# Get the small streamlit model data
+To create images, questions and answer in order to train the small model on streamlit, run
+```bash
+sh faklevr_data_creation/small_faklevr_dataset_creation.sh
+```
---
+++ faklevr_data_creation/small_faklevr_dataset_creation.sh
@@ -0,0 +1,11 @@
+python faklevr_data_creation/small_faklevr_dataset_creation.py \
+  --num_images 10000 \
+  --split train
+
+python faklevr_data_creation/small_faklevr_dataset_creation.py \
+  --num_images 1000 \
+  --split val
+
+python faklevr_data_creation/small_faklevr_dataset_creation.py \
+  --num_images 1000 \
+  --split test
